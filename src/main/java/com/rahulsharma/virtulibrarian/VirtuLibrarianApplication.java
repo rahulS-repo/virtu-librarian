30c8cf4e99ed0c5478da19e2b3fb9c4522b9d16f
rahulS-repo <sharma.rahul201718@gmail.com>
2023-12-29 11:38:45 +0530

    application setup

---
+++ src/main/java/com/rahulsharma/virtulibrarian/VirtuLibrarianApplication.java
@@ -0,0 +1,13 @@
+package com.rahulsharma.virtulibrarian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtuLibrarianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtuLibrarianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahulsharma/virtulibrarian/VirtuLibrarianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahulsharma.virtulibrarian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtuLibrarianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
